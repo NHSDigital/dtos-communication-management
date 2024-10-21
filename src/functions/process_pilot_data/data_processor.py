313a76abc0cf705727cdd18cb845fa845acad999
Steve Laing <steve.laing@gmail.com>
2024-10-21 10:13:58 +0100

    Add blob file processing function
    This commit adds an Azure function which is triggered by blob
    create/update events. The underlying processor reads the blob data
    expecting csv format. The data is then posted to the batch notify
    functioni in the previous commit via HTTP. It also specifies the
    routing plan to be used when forwarding the data as an NHS Notify API call.

---
+++ src/functions/process_pilot_data/data_processor.py
@@ -0,0 +1,72 @@
+import csv
+import logging
+import os
+import requests
+import dateutil.parser
+
+
+class DataProcessor:
+    FIELDNAMES = ("nhs_number", "date_of_birth")
+    HEADERS = {
+        "Content-type": "application/json",
+        "Accept": "application/json",
+    }
+
+    @classmethod
+    def call(cls, raw_data):
+        data = cls(raw_data).__valid_csv_data()
+        if not data:
+            logging.error("No valid data found")
+            return
+        post_body = {
+            "routing_plan": "breast-screening-pilot",
+            "data": data
+        }
+        response = requests.post(cls.url(), json=post_body, headers=cls.HEADERS)
+
+        if response:
+            logging.info(response.text)
+        else:
+            logging.error(response.text)
+
+        return response
+
+    @classmethod
+    def url(cls):
+        return cls.base_url() + "/batch-message/breast-screening-pilot"
+
+    @classmethod
+    def base_url(cls):
+        return os.environ['BASE_URL']
+
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+
+    def __valid_csv_data(self):
+        data = []
+        try:
+            reader = csv.DictReader(self.raw_data, self.FIELDNAMES)
+            for row in reader:
+                if self.__valid_row(row):
+                    data.append(row)
+        except csv.Error:
+            logging.error("Invalid CSV data")
+            return []
+        return data
+
+    def __valid_row(self, row):
+        return self.__valid_nhs_number(row["nhs_number"]) and self.__valid_date_of_birth(row["date_of_birth"])
+
+    def __valid_nhs_number(self, nhs_number):
+        if not nhs_number:
+            return False
+        return len(nhs_number) == 10 and nhs_number.isdigit()
+
+    def __valid_date_of_birth(self, date_of_birth):
+        if not date_of_birth:
+            return False
+        try:
+            dateutil.parser.parse(date_of_birth)
+            return True
+        except ValueError:
+            return False
---
+++ src/functions/process_pilot_data/tests/test_data_processor.py
@@ -0,0 +1,75 @@
+import json
+import pytest
+import requests_mock
+
+from unittest.mock import MagicMock
+from functions.process_pilot_data.data_processor import DataProcessor
+
+
+class TestDataProcessor:
+    @pytest.fixture
+    def setup(self):
+        mock_base_url = MagicMock(name="base_url")
+        mock_base_url.return_value = "http://example.com"
+        DataProcessor.base_url = mock_base_url
+
+    def test_call(self, setup):
+        response_text = json.dumps({"data": "OK"})
+        csv_data = [
+            "0000000000,2001-02-03",
+            "1111111111,2002-04-04",
+            "2222222222,1985-05-04",
+        ]
+
+        expected_request_body = {
+            "routing_plan": "breast-screening-pilot",
+            "data": [
+                {"nhs_number": "0000000000", "date_of_birth": "2001-02-03"},
+                {"nhs_number": "1111111111", "date_of_birth": "2002-04-04"},
+                {"nhs_number": "2222222222", "date_of_birth": "1985-05-04"},
+            ]
+        }
+        with requests_mock.Mocker() as rm:
+            adapter = rm.post(
+                'http://example.com/batch-message/breast-screening-pilot',
+                text=response_text
+            )
+            DataProcessor.call(csv_data)
+
+            assert adapter.called
+            assert adapter.call_count == 1
+            assert adapter.last_request.json() == expected_request_body
+
+    def test_call_with_missing_csv_data(self, setup):
+        response_text = json.dumps({"data": "OK"})
+        csv_data = [
+            "0000000000,yyyy-mm-dd",
+            "1111111111",
+            "2222222222",
+        ]
+
+        with requests_mock.Mocker() as rm:
+            adapter = rm.post(
+                'http://example.com/batch-message/breast-screening-pilot',
+                text=response_text
+            )
+            DataProcessor.call(csv_data)
+
+            assert not adapter.called
+            assert adapter.call_count == 0
+            assert adapter.last_request is None
+
+    def test_call_with_invalid_csv_data(self, setup):
+        response_text = json.dumps({"data": "OK"})
+        invalid_data = "\n"
+
+        with requests_mock.Mocker() as rm:
+            adapter = rm.post(
+                'http://example.com/batch-message/breast-screening-pilot',
+                text=response_text
+            )
+            DataProcessor.call(invalid_data)
+
+            assert not adapter.called
+            assert adapter.call_count == 0
+            assert adapter.last_request is None
