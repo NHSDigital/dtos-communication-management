08d7f67fab4b678670befd7ee775ae4df5841b33
Steve Laing <steve.laing@gmail.com>
2024-10-24 21:24:13 +0100

    Refactor to use explicit import
    Declassify tests
    
    Signed-off-by: Steve Laing <steve.laing@gmail.com>

--- src/functions/process_pilot_data/function_app.py
+++ src/functions/process_pilot_data/function_app.py
@@ -6,7 +6,7 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from helper import process_data
+import helper
 
 BLOB_CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME", "pilot-data")
 
@@ -20,5 +20,5 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def process_pilot_data(csvblob: func.InputStream) -> str:
     logging.info("Triggering data processor from blob update")
     raw_data = csvblob.read().decode("utf-8").splitlines()
-    result = process_data(raw_data)
+    result = helper.process_data(raw_data)
     logging.info(f"Data processor result: {result}")
