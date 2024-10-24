a5bb368963524732798e6e128469e2c3eaa99997
Steve Laing <steve.laing@gmail.com>
2024-10-24 16:48:00 +0100

    Add correlation ID to pilot data

--- src/functions/process_pilot_data/helper.py
+++ src/functions/process_pilot_data/helper.py
@@ -3,6 +3,7 @@ import logging
 import os
 import requests
 import dateutil.parser
+import uuid
 
 FIELDNAMES = ("nhs_number", "date_of_birth", "appointment_date", "appointment_time", "appointment_location", "appointment_type")
 HEADERS = {
@@ -11,7 +12,7 @@ HEADERS = {
 }
 
 
-def process_data(raw_data):
+def process_data(raw_data) -> str:
     data = valid_csv_data(raw_data)
     if not data:
         logging.error("No valid data found")
@@ -27,12 +28,13 @@ def process_data(raw_data):
     return response.text
 
 
-def valid_csv_data(raw_data):
+def valid_csv_data(raw_data) -> list:
     data = []
     try:
         reader = csv.DictReader(raw_data, FIELDNAMES)
         for row in reader:
             if valid_row(row):
+                row["correlation_id"] = str(uuid.uuid4())
                 data.append(row)
     except csv.Error:
         logging.error("Invalid CSV data")
@@ -40,24 +42,28 @@ def valid_csv_data(raw_data):
     return data
 
 
-def valid_row(row):
+def valid_row(row) -> bool:
     return (
         valid_nhs_number(row["nhs_number"]) and
-        valid_date_of_birth(row["date_of_birth"])
+        valid_date_or_time(row["date_of_birth"]) and
+        valid_date_or_time(row["appointment_date"]) and
+        valid_date_or_time(row["appointment_time"]) and
+        row["appointment_location"] and
+        row["appointment_type"]
     )
 
 
-def valid_nhs_number(nhs_number):
+def valid_nhs_number(nhs_number: str) -> bool:
     if not nhs_number:
         return False
     return len(nhs_number) == 10 and nhs_number.isdigit()
 
 
-def valid_date_of_birth(date_of_birth):
-    if not date_of_birth:
+def valid_date_or_time(val: str) -> bool:
+    if not val:
         return False
     try:
-        dateutil.parser.parse(date_of_birth)
+        dateutil.parser.parse(val)
         return True
     except ValueError:
         return False
