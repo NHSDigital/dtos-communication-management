8b13efa8cac988efb1d84bd64521c475f0870aee
Steve Laing <steve.laing@gmail.com>
2024-11-04 09:53:18 +0000

    Add tests init.py files
    These __init__.py files allow the tests to locate the correct functions and associated modules.
    It's also necessary to remove the non-unique 'function_app' module from each test package.
    This clash occurs because of the limitations of Azure function_app.py naming, we can't nest them in a module heirarchy.

---
+++ tests/process_pilot_data/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+if "function_app" in sys.modules:
+    del sys.modules["function_app"]
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../src/functions/process_pilot_data/")
