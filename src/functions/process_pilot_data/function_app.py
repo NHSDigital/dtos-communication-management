e682e9f4359a39a08379b697f59331705187e9ac
Steve Laing <steve.laing@gmail.com>
2024-11-04 11:42:29 +0000

    Remove top level modules (#24)
    * Remove top level modules
    
    Azure functions cannot import modules nested within a top level module.
    Imports from Azure functions assume that function_app.py sits above the
    top level and can them import from any sub packages.
    
    This has consequences for repos like this which contain more than one
    function_app.py as they cannot be differentiated by top level module.
    
    * Disambiguate helper module in one of the functions
    
    As a consequence of removing the top level module we now have a 'helper' module name clash, so rename one of them.
    
    * Move test files to tests/
    
    This isn't strictly necessary as part of the module bug fix but it's probably better to have all our tests in one place.
    
    * Add tests init.py files
    
    These __init__.py files allow the tests to locate the correct functions and associated modules.
    It's also necessary to remove the non-unique 'function_app' module from each test package.
    This clash occurs because of the limitations of Azure function_app.py naming, we can't nest them in a module heirarchy.
    
    * Rename notify helper to notifier
    
    As part of disambiguating our modules, rename the notify helper to something descriptive and unique.

--- src/functions/process_pilot_data/function_app.py
+++ src/functions/process_pilot_data/function_app.py
@@ -1,5 +1,5 @@
 import azure.functions as func
-import process_pilot_data.helper
+import data_processor
 import logging
 import os
 
@@ -11,10 +11,12 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="ProcessPilotData")
 @app.blob_trigger(
-    arg_name="csvblob", path="%BLOB_CONTAINER_NAME%/{name}.csv", connection="AzureWebJobsStorage"
+    arg_name="csvblob",
+    path="%BLOB_CONTAINER_NAME%/{name}.csv",
+    connection="AzureWebJobsStorage",
 )
 def process_data(csvblob: func.InputStream) -> str:
     logging.info("Triggering data processor from blob update")
     raw_data = csvblob.read().decode("utf-8").splitlines()
-    result = process_pilot_data.helper.process_data(raw_data)
+    result = data_processor.process_data(raw_data)
     logging.info(f"Data processor result: {result}")
