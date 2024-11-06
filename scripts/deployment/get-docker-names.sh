a65977522dfd30542577c6ba4aab84da899acbba
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-06 17:02:24 +0000

    feat: DTOSS-4810 Continuous Integration workflow

---
+++ scripts/deployment/get-docker-names.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+declare -A docker_functions_map=(
+    ["functions/process_pilot_data"]="process-pilot-data"
+    ["functions/notify"]="notify"
+)
+
+changed_functions=""
+
+set -x
+
+if [ -z "$CHANGED_FOLDERS" ]; then
+    changed_functions="null"
+    echo "No files changed"
+elif [[ "$CHANGED_FOLDERS" == *Shared* ]]; then
+    echo "Shared folder changed, returning all functions"
+    for key in "${!docker_functions_map[@]}"; do
+        changed_functions+=" ${docker_functions_map[$key]}"
+        echo "Adding in: ${docker_functions_map[$key]}"
+    done
+else
+    echo "files changed $CHANGED_FOLDERS "
+    for folder in $CHANGED_FOLDERS; do
+      echo "Add this function in: ${folder} "
+      echo "Add this which maps to: ${docker_functions_map[$folder]} "
+      changed_functions+=" ${docker_functions_map[$folder]}"
+    done
+fi
+
+# Format the output for the github matrix:
+changed_functions_json=$(printf '["%s"]' "$(echo $changed_functions | sed 's/ /","/g')")
+
+echo "Final list of functions to rebuild:"
+echo "$changed_functions_json"
+
+echo "FUNC_NAMES=$changed_functions_json" >> "$GITHUB_OUTPUT"
