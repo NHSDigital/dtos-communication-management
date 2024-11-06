329f639e436c7b98b45559ec854805445193299a
Steve Laing <steve.laing@gmail.com>
2024-11-06 15:36:30 +0000

    We should currently send 'breast-screening-pilot' as the routing plan value
    This doesn't currently look up the correct routing plan id for sandbox on the notify function side of things. We may have missed this because e2e tests are not currently being run on CI.

--- tests/process_pilot_data/test_data_processor.py
+++ tests/process_pilot_data/test_data_processor.py
@@ -28,7 +28,7 @@ def test_process_data(setup):
     ]
 
     expected_request_body = {
-        "routing_plan": "breast_screening_first_appointment",
+        "routing_plan": "breast-screening-pilot",
         "recipients": [
             {
                 "nhs_number": "0000000000",
