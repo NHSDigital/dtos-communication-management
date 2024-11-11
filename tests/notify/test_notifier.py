2f0b39d189768eaa09df658fcaca52949d696862
Steve Laing <steve.laing@gmail.com>
2024-11-11 19:58:23 +0000

    Integrate JWT access token requests (#29)
    * Handle non-200 reponse status when requesting access token
    
    * Rename oauth2 related env vars
    
    The prefixes for these variable names implied they were used with the Notify API, they are used as part of the OAuth2 access token request.
    
    * Remove originator object from payload
    
    This isn't a valid part of the Notify message payload. We receive an error response when testing on integration.
    
    * Add response status code handling for Notify API call
    
    We expect a 201 response when we successfully send a message. Handle and log anything else.
    
    * Extract access token functions from notifier helper
    
    Obtaining an access token from the OAuth2 token endpoint is a distinct operation from sending notifications.
    This commit extracts jwt access token generation and supporting functions.
    
    * Read private key from env
    
    This key will be populated as an environment variable, the value will be stored in the keyvault.

--- tests/notify/test_notifier.py
+++ tests/notify/test_notifier.py
@@ -1,19 +1,19 @@
 import json
+import logging
 import notifier
 import pytest
 import requests_mock
-import cryptography.hazmat.primitives.asymmetric.rsa as rsa
 import uuid
 import routing_plans
+
 
 @pytest.fixture
 def setup(monkeypatch):
     monkeypatch.setenv("NOTIFY_API_URL", "http://example.com")
-    monkeypatch.setenv("OAUTH2_TOKEN_URL", "http://tokens.example.com")
 
 
 def test_send_messages(mocker):
-    mocker.patch("notifier.get_access_token", return_value="access_token")
+    mocker.patch("access_token.get_token", return_value="an_access_token")
     send_message_mock = mocker.patch("notifier.send_message", return_value="OK")
     data = {
         "routing_plan": "breast-screening-pilot",
@@ -22,30 +22,26 @@ def test_send_messages(mocker):
             {"nhs_number": "0000000001"},
         ]
     }
-    with requests_mock.Mocker() as rm:
-        rm.post(
-            "http://example.com/comms", text="access_token"
-        )
 
-        response = notifier.send_messages(data)
+    response = notifier.send_messages(data)
 
-        assert send_message_mock.call_count == 2
+    assert send_message_mock.call_count == 2
 
-        assert response == "OK\nOK"
-        send_message_mock.assert_any_call(
-            "access_token",
-            routing_plans.get_id("breast-screening-pilot"),
-            {"nhs_number": "0000000000"},
-        )
-        send_message_mock.assert_any_call(
-            "access_token",
-            routing_plans.get_id("breast-screening-pilot"),
-            {"nhs_number": "0000000001"},
-        )
+    assert response == "OK\nOK"
+    send_message_mock.assert_any_call(
+        "an_access_token",
+        routing_plans.get_id("breast-screening-pilot"),
+        {"nhs_number": "0000000000"},
+    )
+    send_message_mock.assert_any_call(
+        "an_access_token",
+        routing_plans.get_id("breast-screening-pilot"),
+        {"nhs_number": "0000000001"},
+    )
 
 
 def test_send_messages_with_individual_routing_plans(mocker):
-    mocker.patch("notifier.get_access_token", return_value="access_token")
+    mocker.patch("access_token.get_token", return_value="an_access_token")
 
     data = {
         "recipients": [
@@ -56,24 +52,20 @@ def test_send_messages_with_individual_routing_plans(mocker):
 
     send_message_mock = mocker.patch("notifier.send_message", return_value="OK")
 
-    with requests_mock.Mocker() as rm:
-        rm.post(
-            "http://example.com/comms", text="access_token"
-        )
-        response = notifier.send_messages(data)
+    response = notifier.send_messages(data)
 
-        assert send_message_mock.call_count == 2
-        assert response == "OK\nOK"
-        send_message_mock.assert_any_call(
-            "access_token",
-            "b838b13c-f98c-4def-93f0-515d4e4f4ee1",
-            {"nhs_number": "0000000000"},
-        )
-        send_message_mock.assert_any_call(
-            "access_token",
-            "b1e3b13c-f98c-4def-93f0-515d4e4f4ee1",
-            {"nhs_number": "0000000001"},
-        )
+    assert send_message_mock.call_count == 2
+    assert response == "OK\nOK"
+    send_message_mock.assert_any_call(
+        "an_access_token",
+        "b838b13c-f98c-4def-93f0-515d4e4f4ee1",
+        {"nhs_number": "0000000000"},
+    )
+    send_message_mock.assert_any_call(
+        "an_access_token",
+        "b1e3b13c-f98c-4def-93f0-515d4e4f4ee1",
+        {"nhs_number": "0000000001"},
+    )
 
 
 def test_send_message(setup):
@@ -123,6 +115,48 @@ def test_send_message(setup):
         assert adapter.call_count == 1
         assert adapter.last_request.json() == expected_request_body
 
+
+def test_send_message_with_error_response(mocker, setup):
+    access_token = "access_token"
+    routing_plan = "breast-screening-pilot"
+    routing_plan_id = routing_plans.get_id(routing_plan)
+    message_data = {
+        "routing_plan": routing_plan,
+        "nhs_number": "0000000000",
+        "date_of_birth": "1981-10-07",
+        "appointment_time": "10:00",
+        "appointment_date": "2021-12-01",
+        "appointment_location": "Breast Screening Clinic, 123 High Street, London",
+        "correlation_id": "da0b1495-c7cb-468c-9d81-07dee089d728",
+        "contact_telephone_number": "012345678",
+    }
+
+    response_text = json.dumps(
+        {
+            "errors": [
+                {
+                    "status": "401",
+                    "title": "Access Denied",
+                    "detail": "Access token missing",
+                },
+            ],
+        },
+    )
+
+    error_logging_spy = mocker.spy(logging, "error")
+
+    with requests_mock.Mocker() as rm:
+        adapter = rm.post(
+            "http://example.com/comms/v1/messages", status_code=401, text=response_text
+        )
+        result = notifier.send_message(access_token, routing_plan_id, message_data)
+
+        assert adapter.called
+        assert adapter.call_count == 1
+        assert result == response_text
+        error_logging_spy.assert_called_once_with(response_text)
+
+
 def test_message_body():
     routing_plan_id = str(uuid.uuid4())
 
@@ -155,26 +189,8 @@ def test_message_body():
                     "tracking_id": "0000000000",
                     "contact_telephone_number": "012345678",
                 },
-                "originator": {
-                    "odsCode": "X26",
-                },
             },
         }
     }
 
     assert actual == expected
-
-
-def test_get_access_token(monkeypatch, mocker, setup):
-    monkeypatch.setenv("NOTIFY_API_KEY", "an_api_key")
-    monkeypatch.setenv("NOTIFY_API_KID", "a_kid")
-    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    mocker.patch("notifier.get_private_key", return_value=private_key)
-
-    with requests_mock.Mocker() as rm:
-        rm.post(
-            "http://tokens.example.com/",
-            json={"access_token": "an_access_token"},
-        )
-        access_token = notifier.get_access_token()
-        assert access_token == "an_access_token"
