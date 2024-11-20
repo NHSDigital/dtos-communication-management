64c82dd5e5fad89eb7cec57148fa81b9b40b38f1
Steve Laing <steve.laing@gmail.com>
2024-11-20 11:09:12 +0000

    Remove logging from integration test run
    Testing error scenarios generates log output which are easily mistaken as test failures.

--- test-integration.sh
+++ test-integration.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker compose --env-file .env.test run --build integration-tests pytest -vv --log-cli-level=INFO /tests/integration
+docker compose --env-file .env.test run --build integration-tests pytest /tests/integration
 docker compose --env-file .env.test down db --volumes
-
