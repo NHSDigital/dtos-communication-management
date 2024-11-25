a1d57936199271d15175abc4f7c1bed6a882723a
Nimmo <dnimmo@gmail.com>
2024-11-25 23:49:07 +0000

    added human_readable_date and created private _to_format function to avoid repeated code

--- src/shared/format_date.py
+++ src/shared/format_date.py
@@ -1,16 +1,25 @@
 from datetime import datetime
 
-def to_date_of_birth(date_str: str) -> str | None:
+crystal_report_extract_format = "%dM%mM%Y"
+
+def _to_format(date_str: str, desired_format: str) -> str | None:
     try:
-        # If we can parse the date string as a date, it's already in the correct format - return as-is
-        datetime.strptime(date_str, "%Y-%m-%d")
+        # If we can parse the date string in the desired format, we can return it as-is
+        datetime.strptime(date_str, desired_format)
         return date_str
     except ValueError:
         pass
 
     try:
-        return datetime.strptime(date_str, "%dM%mM%Y").strftime("%Y-%m-%d")
+        return datetime.strptime(date_str, crystal_report_extract_format).strftime(desired_format)
     except ValueError:
         return None
 
-# STAGE 2 - Incident clients booked into specified clinic(s) and date(s),1234567890,"BLAKE, KENNETH, MR",07M11M1989,EP700,25M11M2024,11:56:00,Edgware Prospect Clinics
+
+def to_date_of_birth(date_str: str) -> str | None:
+    return _to_format(date_str, "%Y-%m-%d")
+
+
+def to_human_readable_date(date_str: str) -> str | None:
+    return _to_format(date_str, "%d/%m/%Y")
+
