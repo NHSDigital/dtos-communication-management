784d34aa2da23bee8094a697577560e17cd2d3d1
Steve Laing <steve.laing@gmail.com>
2024-11-25 11:55:28 +0000

    Adjust test script config to output coverage

--- test-pacts.sh
+++ test-pacts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pytest -W ignore::PendingDeprecationWarning -cov=src -vv tests/pacts || {
+pytest -W ignore::PendingDeprecationWarning -vv tests/pacts || {
     echo "Tests failed in tests/pacts"
     exit 1
 }
