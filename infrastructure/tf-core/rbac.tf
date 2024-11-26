99c084e49ec2b42b9dc6baa64c98f5d874750a25
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-26 12:29:13 +0000

    fix: DNS zone changes in hub

--- infrastructure/tf-core/rbac.tf
+++ infrastructure/tf-core/rbac.tf
@@ -12,3 +12,14 @@ locals {
   ]
 
 }
+
+module "user_rbac_assignments" {
+  # This results in a numbered index, necessary to avoid keying on values that are only known after apply (scope is a resource id)
+  for_each = { for idx, assignment in var.rbac_role_assignments : idx => assignment }
+
+  source = "../../../dtos-devops-templates/infrastructure/modules/rbac-assignment"
+
+  principal_id         = azurerm_linux_function_app.function_app.identity.0.principal_id
+  role_definition_name = each.value.role_definition_name
+  scope                = each.value.scope
+}
