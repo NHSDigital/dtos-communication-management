2af1ad916300b924d27b36acf58b3bfae6e1a1c5
Nimmo <dnimmo@gmail.com>
2024-11-27 23:27:37 +0000

    update code to match tests

--- src/shared/format_time.py
+++ src/shared/format_time.py
@@ -6,9 +6,15 @@ def to_human_readable_twelve_hours(raw_time: str) -> str | None:
     Convert a time string like '11:56:00' to '11:56am' or '12:56pm'.
     """
     try:
+        if raw_time is None:
+            logging.error("Input is None.")
+            return None
+
+        raw_time = raw_time.strip()
+
         time_obj = datetime.strptime(raw_time, "%H:%M:%S")
         return time_obj.strftime("%I:%M%p").lower()
 
-    except ValueError:
+    except (ValueError, TypeError):
         logging.error(f"Invalid time format: {raw_time}")
         return None
