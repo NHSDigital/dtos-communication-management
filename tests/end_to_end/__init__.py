0e21a8e90e1370d81ae3947ec96fee485f5cba52
Steve Laing <steve.laing@gmail.com>
2024-12-03 16:18:09 +0000

    Add message status integration tests (#86)
    * Add message status recorder database integration test
    
    * Call message_status_recorder to save message statuses
    
    If the HMAC signature in the request is verified, call the message status recorder to save the status of the messages.
    
    * Add an end to end test for saving callback data to database

--- tests/end_to_end/__init__.py
+++ tests/end_to_end/__init__.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# Import order is important here as we need to load the message status function_app module first.
 sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../src/functions/message_status/")
 sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../src/functions/notify/")
 sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../src/shared/")
