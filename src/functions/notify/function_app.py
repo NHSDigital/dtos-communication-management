024b1fd51ff108e8cdcc2f393f24d7e244617fb8
Steve Laing <steve.laing@gmail.com>
2024-12-05 09:30:17 +0000

    Add logging for initialization of each function
    This gives us a trigger timestamp in our logs.

--- src/functions/notify/function_app.py
+++ src/functions/notify/function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import json
+import logging
 import notifier
 
 app = func.FunctionApp()
@@ -8,6 +9,7 @@ app = func.FunctionApp()
 @app.function_name(name="Notify")
 @app.route(route="notify/{notification_type}/send", auth_level=func.AuthLevel.ANONYMOUS)
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Notify HTTP trigger function. Processing batch notification.")
     notification_type: str = req.route_params.get("notification_type")
     req_body_bytes: bytes = req.get_body()
     json_body: str = json.loads(req_body_bytes.decode("utf-8"))
