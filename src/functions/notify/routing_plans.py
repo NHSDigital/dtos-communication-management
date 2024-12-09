d2c111da0cde00dead60c67e4ab620dd2489ed55
Nimmo <dnimmo@gmail.com>
2024-12-09 14:18:08 +0000

    added routing plan

--- src/functions/notify/routing_plans.py
+++ src/functions/notify/routing_plans.py
@@ -1,7 +1,8 @@
 import os
 
 breast_screening_first_appointment_plans = {
-    "sandbox": "b838b13c-f98c-4def-93f0-515d4e4f4ee1"
+    "sandbox": "b838b13c-f98c-4def-93f0-515d4e4f4ee1",
+    "integration": "97eaad14-a72c-45f3-bbcd-c0071113c1c2"
 }
 
 bowel_screening_first_appointment_plans = {
@@ -14,4 +15,4 @@ ROUTING_PLANS = {
 }
 
 def get_id(key: str) -> str | None:
-    return ROUTING_PLANS.get(key)[os.getenv("ENVIRONMENT") or "sandbox"]
+    return ROUTING_PLANS.get(key)[(os.getenv("ENVIRONMENT", "sandbox").lower())]
