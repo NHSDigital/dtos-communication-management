8bcb9ab146b90200380da96550a38ba953191b0d
Steve Laing <steve.laing@gmail.com>
2024-12-09 11:39:27 +0000

    Restrict Notify function http methods
    Remove the dynamic portion of the path, we don't use it.

--- src/functions/notify/function_app.py
+++ src/functions/notify/function_app.py
@@ -7,12 +7,14 @@ app = func.FunctionApp()
 
 
 @app.function_name(name="Notify")
-@app.route(route="notify/{notification_type}/send", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(
+    route="notify/message/send",
+    auth_level=func.AuthLevel.ANONYMOUS,
+    methods=[func.HttpMethod.POST],
+)
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Notify HTTP trigger function. Processing batch notification.")
-    notification_type: str = req.route_params.get("notification_type")
     req_body_bytes: bytes = req.get_body()
     json_body: str = json.loads(req_body_bytes.decode("utf-8"))
 
-    if notification_type == "message":
-        return notifier.send_messages(json_body)
+    return notifier.send_messages(json_body)
