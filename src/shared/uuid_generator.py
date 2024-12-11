095d629b6469f9030f9dcfe1a6e5ca5495e337ed
Steve Laing <steve.laing@gmail.com>
2024-12-11 13:56:50 +0000

    Remove message_reference method
    The same functionality will be implemented in recipient_id - a repeatable hash of appt. time, date, NiN and DoB.

--- src/shared/uuid_generator.py
+++ src/shared/uuid_generator.py
@@ -1,21 +1,14 @@
 import hashlib
+import time
 import uuid
 
 
 def recipient_id(message_data: dict) -> str:
     str_val: str = ",".join([
-        message_data["nhs_number"],
-        message_data["date_of_birth"],
-    ])
-    return reference_uuid(str_val)
-
-
-def message_reference(message_data: dict) -> str:
-    str_val: str = ",".join([
-        message_data["nhs_number"],
-        message_data["date_of_birth"],
         message_data["appointment_date"],
         message_data["appointment_time"],
+        message_data["date_of_birth"],
+        message_data["nhs_number"],
     ])
     return reference_uuid(str_val)
 
--- tests/unit/shared/test_uuid_generator.py
+++ tests/unit/shared/test_uuid_generator.py
@@ -7,21 +7,12 @@ def test_reference_uuid():
     assert uuid_generator.reference_uuid(val) == "098f6bcd-4621-d373-cade-4e832627b4f6"
 
 
-def test_message_reference():
-    """Tests the predictable conversion of some message data stored in a dict to a UUID."""
-    message_data = {
-        "nhs_number": "1234567890",
-        "date_of_birth": "01/01/1970",
-        "appointment_date": "01/01/2021",
-        "appointment_time": "12:00",
-    }
-    assert uuid_generator.message_reference(message_data) == "65ee3cb2-35b1-a9df-0a7a-574300738e0e"
-
-
 def test_recipient_id():
     """Tests the predictable conversion of a dict containing nhs_number and date_of_birth to a UUID."""
     message_data = {
+        "appointment_date": "2025-02-01",
+        "appointment_time": "12:00",
+        "date_of_birth": "1970-01-01",
         "nhs_number": "1234567890",
-        "date_of_birth": "01/01/1970",
     }
-    assert uuid_generator.recipient_id(message_data) == "b2e5a632-111c-a342-7246-42482f9cab5c"
+    assert uuid_generator.recipient_id(message_data) == "43b1d23b-42b5-9700-e677-1159c15d378f"
