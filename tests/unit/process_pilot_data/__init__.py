50668697d6d64c845c6b0877ec1a6d8ffabcac01
Steve Laing <steve.laing@gmail.com>
2024-12-11 12:48:55 +0000

    Remove data mutation from data processor (#117)
    * Build a new row for each recipient
    
    We should not mutate the data being passed to this function. By creating a new row of data for the array we return we also drop any unnecessary fields implicitly.
    
    * Add shared dir to unit test dir
    
    This allows us to run a single unit test dependent on shared code.

--- tests/unit/process_pilot_data/__init__.py
+++ tests/unit/process_pilot_data/__init__.py
@@ -6,3 +6,4 @@ if "function_app" in sys.modules:
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../../src/functions/process_pilot_data/")
+sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../../src/shared/")
