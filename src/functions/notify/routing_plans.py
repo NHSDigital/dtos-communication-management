394a58ea94e8013904c63f07d4bb48789956bf6d
Steve Laing <steve.laing@gmail.com>
2024-12-12 12:03:24 +0000

    Add production routing plan ID (#122)
    We now have several duplicate entries and will revisit how we structure these, but this is currently what is needed to run the pilot in production.

--- src/functions/notify/routing_plans.py
+++ src/functions/notify/routing_plans.py
@@ -4,6 +4,7 @@ breast_screening_first_appointment_plans = {
     "development": "b838b13c-f98c-4def-93f0-515d4e4f4ee1",
     "integration": "97eaad14-a72c-45f3-bbcd-c0071113c1c2",
     "nft": "97eaad14-a72c-45f3-bbcd-c0071113c1c2",
+    "production": "97eaad14-a72c-45f3-bbcd-c0071113c1c2",
 }
 
 bowel_screening_first_appointment_plans = {
