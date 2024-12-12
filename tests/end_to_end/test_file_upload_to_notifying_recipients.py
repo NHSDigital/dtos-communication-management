73965c1ae1fe673b5692c90446457b2d226c3282
Nimmo <dnimmo@gmail.com>
2024-12-12 08:50:52 +0000

    DTOS-6381 - account for SX Code in CSV (#120)
    * test updated for addition of SX Code
    
    * data processor updated for addition of SX Code
    
    * e2e tests updated

--- tests/end_to_end/test_file_upload_to_notifying_recipients.py
+++ tests/end_to_end/test_file_upload_to_notifying_recipients.py
@@ -24,8 +24,8 @@ def upload_file_to_blob_storage():
         pilot_data_client = blob_service_client.get_container_client(blob_container_name)
         blob_client = pilot_data_client.get_blob_client("HWA NHS App Pilot 002 SPRPT.csv")
         csv_data = "\n".join([
-            'UNUSED_STAGE_COLUMN,9990548609,"BLAKE, KYLIE, MRS",07M09M1971,EP700,01M12M2024,10:15:00,"The Royal Shrewsbury Hospital, Breast Screening Office, Treatment Centre, Mytton Oak Road, Shrewsbury, SY3 8XQ"',
-            'UNUSED_STAGE_COLUMN,9435732992,"BLAKE, KAREN, MRS",04M02M1980,EP700,03M01M2025,11:25:00,"The Epping Breast Screening Unit, St Margaret\'s Hospital, The Plain, Epping, Essex, CM16 6TN"',
+            'UNUSED_STAGE_COLUMN,9990548609,987654,"BLAKE, KYLIE, MRS",07M09M1971,EP700,01M12M2024,10:15:00,"The Royal Shrewsbury Hospital, Breast Screening Office, Treatment Centre, Mytton Oak Road, Shrewsbury, SY3 8XQ"',
+            'UNUSED_STAGE_COLUMN,9435732992,987654,"BLAKE, KAREN, MRS",04M02M1980,EP700,03M01M2025,11:25:00,"The Epping Breast Screening Unit, St Margaret\'s Hospital, The Plain, Epping, Essex, CM16 6TN"',
         ]) + "\n"
         content_settings = azure.storage.blob.ContentSettings(content_type='text/csv')
         blob_client.upload_blob(csv_data, blob_type="BlockBlob", content_settings=content_settings, overwrite=True)
