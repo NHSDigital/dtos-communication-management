855a627327a315301c56a23ce71ea471bed35436
Nimmo <dnimmo@gmail.com>
2024-12-12 14:09:15 +0000

    PR comments

--- src/shared/format_time.py
+++ src/shared/format_time.py
@@ -10,10 +10,10 @@ def to_human_readable_twelve_hours(raw_time: str) -> str | None:
             logging.error("Attempted to convert time format, but input is None.")
             return None
 
-        raw_time = raw_time.strip().lstrip("0")
+        raw_time = raw_time.strip()
 
         time_obj = datetime.strptime(raw_time, "%H:%M:%S")
-        return time_obj.strftime("%I:%M%p").lower()
+        return time_obj.strftime("%-I:%M%p").lower()
 
     except (ValueError, TypeError):
         logging.error(f"Invalid time format: {raw_time}")
