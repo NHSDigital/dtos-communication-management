cea82a278eac855ac3c7353ed2db429c2fd0992e
Nimmo <dnimmo@gmail.com>
2024-12-18 05:32:33 +0000

    merge from main

--- test-integration.sh
+++ test-integration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker compose --env-file .env.test --profile test-integration run --build integration-tests pytest -v /tests/integration
+docker compose --env-file .env.test --profile test-integration run --build integration-tests pytest -v --cov=src --cov-append /tests/integration
 test_exit_code=$?
 docker compose --env-file .env.test --profile test-integration down db --volumes
 exit $test_exit_code
--- test-pacts.sh
+++ test-pacts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pytest -W ignore::PendingDeprecationWarning -vv tests/pacts || {
+pytest -W ignore::PendingDeprecationWarning -vv --cov=src --cov-append tests/pacts || {
     echo "Tests failed in tests/pacts"
     exit 1
 }
