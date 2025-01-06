a13ccc966e5548c4ba33419234cb7309d9ca3d79
Steve Laing <steve.laing@gmail.com>
2025-01-06 17:53:22 +0000

    Add function app which delegates requests to Flask

--- dev.sh
+++ dev.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker compose --env-file .env.local --profile dev up --build
+# Install Azure Functions Core Tools before running
+# See: https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-cli-python?pivots=python-mode-decorators&tabs=linux%2Cbash%2Cazure-cli%2Cbrowser#install-the-azure-functions-core-tools
+# Add the relevant package repository then run:
+# sudo apt-get install azure-functions-core-tools-4
+
+cd src/notify && func start --verbose
