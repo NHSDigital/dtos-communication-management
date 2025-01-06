dce06c86726c4a310ca06ff61c5c3d428afcd84f
Steve Laing <steve.laing@gmail.com>
2025-01-06 17:51:52 +0000

    Test Flask app in CI

--- test-integration.sh
+++ test-integration.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-docker compose --env-file .env.test --profile test-integration run --build integration-tests pytest -v /tests/integration
-test_exit_code=$?
-docker compose --env-file .env.test --profile test-integration down db --volumes
-exit $test_exit_code
+
+pytest -vv tests/integration || {
+    echo "Tests failed in tests/integration"
+    exit 1
+}
