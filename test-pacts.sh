c0621f73a5f6923845480430b35398faeef33e48
Steve Laing <steve.laing@gmail.com>
2025-01-06 17:54:02 +0000

    Remove code not currently in use on feature branch

--- test-pacts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pytest -W ignore::PendingDeprecationWarning -vv tests/pacts || {
-    echo "Tests failed in tests/pacts"
-    exit 1
-}
--- tests/unit/notify/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if "function_app" in sys.modules:
-    del sys.modules["function_app"]
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../../src/functions/notify/")
-sys.path.insert(0, os.path.dirname(SCRIPT_DIR) + "/../../src/shared/")
