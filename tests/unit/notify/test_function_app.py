51b3f0254e2dc0c635f1923fc271473ff3e68880
Steve Laing <steve.laing@gmail.com>
2025-01-06 17:53:22 +0000

    Add function app which delegates requests to Flask

---
+++ tests/unit/notify/test_function_app.py
@@ -0,0 +1,24 @@
+from unittest.mock import MagicMock
+import azure.functions as func
+from function_app import main
+import json
+
+
+def test_function_app_calls_flask_app():
+    """Test that the function calls the flask app."""
+    req = func.HttpRequest(
+        method="GET",
+        body=b"",
+        url="/api/status/health-check",
+    )
+    mock_context = MagicMock(
+        function_directory=".",
+        invocation_id="123",
+        function_name="Notify"
+    )
+
+    func_call = main.build().get_user_function()
+    resp = func_call(req, mock_context)
+
+    assert resp.status_code == 200
+    assert json.loads(resp.get_body()) == {"status": "healthy"}
