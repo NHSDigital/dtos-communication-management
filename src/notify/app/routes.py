4d9a72ddf7e029d61fe34d61efed3d0d915158cc
Steve Laing <steve.laing@gmail.com>
2025-01-14 16:56:56 +0000

    Add POST status create endpoint

--- src/notify/app/routes.py
+++ src/notify/app/routes.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, jsonify
+from flask import request
+import json
+import app.lib.request_verifier as request_verifier
+
 
 api = Blueprint("api", __name__)
 
@@ -6,3 +10,19 @@ api = Blueprint("api", __name__)
 @api.route("/status/health-check", methods=["GET"])
 def status_health_check():
     return jsonify({"status": "healthy"}), 200
+
+
+@api.route("/status/create", methods=["POST"])
+def create_status():
+    if request_verifier.verify_headers(dict(request.headers)) is False:
+        status_code = 401
+        body = {"status": "error"}
+    elif request_verifier.verify_signature(dict(request.headers), json.dumps(request.form)):
+        # status_recorder.save_statuses(body_dict)
+        status_code = 200
+        body = {"status": "success"}
+    else:
+        status_code = 403
+        body = {"status": "error"}
+
+    return body, status_code
