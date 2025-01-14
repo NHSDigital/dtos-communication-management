f7f565ad8baeac93635c911f1a4557b3f43bc833
Steve Laing <steve.laing@gmail.com>
2025-01-14 16:55:58 +0000

    Access headers with lowercase keys
    Flask titlecases the keys in Headers, make sure we lowercase before attempting access.

--- src/notify/app/lib/request_verifier.py
+++ src/notify/app/lib/request_verifier.py
@@ -7,17 +7,20 @@ SIGNATURE_HEADER_NAME = 'x-hmac-sha256-signature'
 
 
 def verify_headers(headers: dict) -> bool:
-    if (headers.get(API_KEY_HEADER_NAME) is None or
-            headers.get(API_KEY_HEADER_NAME) != os.getenv('NOTIFY_API_KEY')):
+    lc_headers = header_keys_to_lower(headers)
+    if (lc_headers.get(API_KEY_HEADER_NAME) is None or
+            lc_headers.get(API_KEY_HEADER_NAME) != os.getenv('NOTIFY_API_KEY')):
         return False
 
-    if headers.get(SIGNATURE_HEADER_NAME) is None:
+    if lc_headers.get(SIGNATURE_HEADER_NAME) is None:
         return False
 
     return True
 
 
 def verify_signature(headers: dict, body: str) -> bool:
+    lc_headers = header_keys_to_lower(headers)
+
     expected_signature = hmac.new(
         bytes(signature_secret(), 'ASCII'),
         msg=bytes(body, 'ASCII'),
@@ -26,9 +29,13 @@ def verify_signature(headers: dict, body: str) -> bool:
 
     return hmac.compare_digest(
         expected_signature,
-        headers[SIGNATURE_HEADER_NAME],
+        lc_headers[SIGNATURE_HEADER_NAME],
     )
 
 
 def signature_secret() -> str:
     return f"{os.getenv('APPLICATION_ID')}.{os.getenv('NOTIFY_API_KEY')}"
+
+
+def header_keys_to_lower(headers: dict) -> dict:
+    return {k.lower(): v for k, v in headers.items()}
