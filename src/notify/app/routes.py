8106a593b36ed9146654436c369556e0dbdefae0
Steve Laing <steve.laing@gmail.com>
2025-01-15 12:46:02 +0000

    Move status endpoints to route handlers
    Make healthcheck a top level endpoint, it should not be nested in the
    sttaus/ route.

--- src/notify/app/routes.py
+++ src/notify/app/routes.py
@@ -1,28 +1,16 @@
 from flask import Blueprint, jsonify
-from flask import request
-import json
-import app.lib.request_verifier as request_verifier
+
+import app.route_handlers.status
 
 
 api = Blueprint("api", __name__)
 
 
-@api.route("/status/health-check", methods=["GET"])
+@api.route("/healthcheck", methods=["GET"])
 def status_health_check():
     return jsonify({"status": "healthy"}), 200
 
 
 @api.route("/status/create", methods=["POST"])
 def create_status():
-    if request_verifier.verify_headers(dict(request.headers)) is False:
-        status_code = 401
-        body = {"status": "error"}
-    elif request_verifier.verify_signature(dict(request.headers), json.dumps(request.form)):
-        # status_recorder.save_statuses(body_dict)
-        status_code = 200
-        body = {"status": "success"}
-    else:
-        status_code = 403
-        body = {"status": "error"}
-
-    return body, status_code
+    return app.route_handlers.status.create()
