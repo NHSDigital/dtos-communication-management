97472a445bed367a2d96156b53366bcee78de3d1
Steve Laing <steve.laing@gmail.com>
2025-01-20 16:47:11 +0000

    Refactor persistence tests
    We now have local dev and test dbs so improve tests by asserting persistence has worked as expected.

--- tests/integration/notify/app/route_handlers/test_status_create_endpoint.py
+++ tests/integration/notify/app/route_handlers/test_status_create_endpoint.py
@@ -1,5 +1,7 @@
 from app import create_app
 from app.validators.request_validator import API_KEY_HEADER_NAME, SIGNATURE_HEADER_NAME, signature_secret
+from datetime import datetime, timedelta
+import app.utils.database as database
 import app.utils.hmac_signature as hmac_signature
 import hashlib
 import hmac
@@ -54,3 +56,21 @@ def test_status_create_request_validation_succeeds(setup, client, message_status
 
     assert response.status_code == 200
     assert response.get_json() == {"status": "success"}
+
+
+def test_status_create_saves_records(setup, client, message_status_post_body):
+    """Test that valid requests are saved to the database."""
+    signature = hmac_signature.create_digest(signature_secret(), json.dumps(message_status_post_body, sort_keys=True))
+
+    headers = {API_KEY_HEADER_NAME: "api_key", SIGNATURE_HEADER_NAME: signature}
+
+    response = client.post('/api/status/create', json=message_status_post_body, headers=headers)
+
+    assert response.status_code == 200
+
+    with database.connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM message_statuses")
+            record = cursor.fetchone()
+            assert record[0] - datetime.now() < timedelta(seconds=1)
+            assert record[1] == message_status_post_body
