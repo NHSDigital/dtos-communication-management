d7552ba5ad46abf34266dc3345e2f837fe9d14b5
Steve Laing <steve.laing@gmail.com>
2025-01-27 14:25:03 +0000

    Simplify db setup
    DB creation is handled nicely in the Alembic script, so remove from CI workflow steps and local scripts.

--- dev.sh
+++ dev.sh
@@ -5,4 +5,12 @@
 # Add the relevant package repository then run:
 # sudo apt-get install azure-functions-core-tools-4
 
+ENV_FILE=".env.local"
+
+source ${ENV_FILE}
+
+echo "Set up database and migrate to latest version..."
+ENV_FILE=${ENV_FILE} alembic upgrade head
+
+echo "Starting the API function app..."
 cd src/notify && func start --verbose
