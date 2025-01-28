88904c6fa3494173339ba10df3963af8806c5486
Steve Laing <steve.laing@gmail.com>
2025-01-28 14:53:42 +0000

    Replace SQL persistence with SQLAlchemy ORM functions

--- tests/unit/notify/app/services/test_status_recorder.py
+++ tests/unit/notify/app/services/test_status_recorder.py
@@ -1,56 +1,40 @@
 import json
+import app.utils.database as database
 import app.services.status_recorder as status_recorder
+import database.models as models
+from sqlalchemy.sql.expression import select
+from sqlalchemy.orm import Session
 
 
-def test_save_statuses_with_channel_status_data(mocker, channel_status_post_body):
-    """Test saving channel status data to datastore"""
-    mock_datastore = mocker.patch("app.services.status_recorder.datastore")
-
+def test_save_statuses_with_channel_status_data(channel_status_post_body):
+    """Test saving channel status data to database"""
     assert status_recorder.save_statuses(channel_status_post_body)
+    channel_status_data = channel_status_post_body["data"][0]
 
-    mock_datastore.create_status_record.assert_called_once_with("ChannelStatus", {
-        "details": json.dumps(channel_status_post_body),
-        "idempotency_key": "2515ae6b3a08339fba3534f3b17cd57cd573c57d25b25b9aae08e42dc9f0a445", #gitleaks:allow
-        "message_id": "2WL3qFTEFM0qMY8xjRbt1LIKCzM",
-        "message_reference": "1642109b-69eb-447f-8f97-ab70a74f5db4",
-        "status": "delivered"
-    })
+    with Session(database.engine()) as session:
+        status_record = session.scalars(select(models.ChannelStatus)).all()[0]
+        assert status_record.details == json.dumps(channel_status_post_body, sort_keys=True)
+        assert status_record.idempotency_key == channel_status_data["meta"]["idempotencyKey"]
+        assert status_record.message_id == channel_status_data["attributes"]["messageId"]
+        assert str(status_record.message_reference) == channel_status_data["attributes"]["messageReference"]
+        assert status_record.status == models.ChannelStatuses(channel_status_data["attributes"]["channelStatus"])
 
 
-def test_save_statuses_with_message_status_data(mocker, message_status_post_body):
+def test_save_statuses_with_message_status_data(message_status_post_body):
     """Test saving message status data to datastore"""
-    mock_datastore = mocker.patch("app.services.status_recorder.datastore")
-
     assert status_recorder.save_statuses(message_status_post_body)
+    message_status_data = message_status_post_body["data"][0]
 
-    mock_datastore.create_status_record.assert_called_once_with("MessageStatus", {
-        "details": json.dumps(message_status_post_body),
-        "idempotency_key": "2515ae6b3a08339fba3534f3b17cd57cd573c57d25b25b9aae08e42dc9f0a445", #gitleaks:allow
-        "message_id": "2WL3qFTEFM0qMY8xjRbt1LIKCzM",
-        "message_reference": "1642109b-69eb-447f-8f97-ab70a74f5db4",
-        "status": "sending"
-    })
-
-
-def test_status_params(message_status_post_body):
-    """Test conversion of request body to message status parameters"""
-    expected = [
-        {
-            "details": json.dumps(message_status_post_body),
-            "idempotency_key": "2515ae6b3a08339fba3534f3b17cd57cd573c57d25b25b9aae08e42dc9f0a445", #gitleaks:allow
-            "message_id": "2WL3qFTEFM0qMY8xjRbt1LIKCzM",
-            "message_reference": "1642109b-69eb-447f-8f97-ab70a74f5db4",
-            "status": "sending"
-        },
-    ]
-
-    assert status_recorder.status_params(message_status_post_body) == expected
+    with Session(database.engine()) as session:
+        status_record = session.scalars(select(models.MessageStatus)).all()[0]
+        assert status_record.details == json.dumps(message_status_post_body, sort_keys=True)
+        assert status_record.idempotency_key == message_status_data["meta"]["idempotencyKey"]
+        assert status_record.message_id == message_status_data["attributes"]["messageId"]
+        assert str(status_record.message_reference) == message_status_data["attributes"]["messageReference"]
+        assert status_record.status == models.MessageStatuses(message_status_data["attributes"]["messageStatus"])
 
 
-def test_status_params_with_missing_field(message_status_post_body):
-    """Test conversion of request body with missing field to message status parameters"""
-    message_status_post_body["data"][0]["attributes"].pop("messageReference")
-
-    expected = []
-
-    assert status_recorder.status_params(message_status_post_body) == expected
+def test_save_statuses_with_error(mocker, channel_status_post_body):
+    """Test saving status data to database with error"""
+    mocker.patch("app.services.status_recorder.Session", side_effect=Exception)
+    assert not status_recorder.save_statuses(channel_status_post_body)
