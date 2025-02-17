5b842077a9eca8b6b4b42145ba3801d2dd40fb1c
Steve Laing <steve.laing@gmail.com>
2025-02-17 10:21:02 +0000

    Fix import deprecation warning for declarative base

--- src/notify/app/models.py
+++ src/notify/app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, ForeignKey, func, Integer, String, Text, TIMESTAMP
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.dialects.postgresql import JSONB, UUID
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 import enum
 
 Base = declarative_base()
