04ef72d7b8f9d7eee1fc1b38f4d9110a248855bd
Harriet H-W <harriethw@gmail.com>
2025-07-09 15:43:50 +0100

    add a unit test for the migrate function
    This has very little value as there is already an
    integration test, but it was a learning exercise
    for me but does tick a box while I'm looking at
    this file anyway.

--- tests/unit/notify/test_function_app.py
+++ tests/unit/notify/test_function_app.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock
 import azure.functions as func
-from function_app import main
+from function_app import main, migrate_database
 import json
 
 
@@ -22,3 +22,25 @@ def test_function_app_calls_flask_app():
 
     assert resp.status_code == 200
     assert json.loads(resp.get_body()) == {"status": "healthy"}
+
+
+def test_function_app_migrates(mocker, monkeypatch):
+    """Test that the function calls to migrate."""
+    monkeypatch.setenv("DATABASE_PASSWORD", "password")
+    mocked_migrate = mocker.patch(
+        "function_app.alembic_migrate",
+        return_value=("success")
+    )
+    mocked_migrate.start()
+
+    req = func.HttpRequest(
+        method="POST",
+        body=json.dumps({}).encode(),
+        headers={"x-migration-key": "password"},
+        url="/api/migrate-database",
+    )
+
+    func_call = migrate_database.build().get_user_function()
+    func_call(req)
+
+    mocked_migrate.assert_called_once()
