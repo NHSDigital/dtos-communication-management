d2569551c1470239fd577277b6e04bd44acf1eab
Harriet H-W <harriet.horobin-worley1@nhs.net>
2025-07-11 16:59:13 +0100

    WIP add optional message origin to batch requests
    if an origin is given we find or create a new one
    still to do:
    * check error handling around creating/finding origin?

--- tests/unit/notify/app/services/test_message_batch_recorder.py
+++ tests/unit/notify/app/services/test_message_batch_recorder.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql.expression import select
 from sqlalchemy.orm import Session
 
 
-def test_save_batch(message_batch_post_body, message_batch_post_response):
+def test_save_batch_without_origin(message_batch_post_body, message_batch_post_response):
     """When save_batch is called with a valid batch, the batch and messages should be saved."""
     success, response = message_batch_recorder.save_batch(
         message_batch_post_body,
@@ -31,6 +31,38 @@ def test_save_batch(message_batch_post_body, message_batch_post_response):
         assert messages[0].message_id == message_batch_post_response["data"]["attributes"]["messages"][0]["id"]
         assert str(messages[0].message_reference) == message_batch_post_response["data"]["attributes"]["messages"][0]["messageReference"]
         merged_messages = message_batch_recorder.merged_messages(message_batch_post_body, message_batch_post_response)
+        assert messages[0].details == merged_messages[0]
+        assert messages[0].nhs_number == merged_messages[0]["recipient"]["nhsNumber"]
+
+def test_save_batch_with_new_origin_key(message_batch_post_body_with_origin, message_batch_post_response_with_origin):
+    """When save_batch is called with a valid batch, the batch and messages should be saved."""
+    success, response = message_batch_recorder.save_batch(
+        message_batch_post_body_with_origin,
+        message_batch_post_response_with_origin,
+        models.MessageBatchStatuses.SENT
+    )
+    assert success
+    assert response == "Batch id: 1 saved successfully"
+
+    with Session(database.engine()) as session:
+        message_origin = session.scalars(select(models.MessageOrigin)).all()[0]
+        assert message_origin.key ==  message_batch_post_response_with_origin["data"]["messageOriginKey"]
+
+        message_batch = session.scalars(select(models.MessageBatch)).all()[0]
+        messages = session.scalars(select(models.Message)).all()
+
+        assert message_batch.id == messages[0].batch_id
+        assert message_batch.batch_id == message_batch_post_response_with_origin["data"]["id"]
+        assert str(message_batch.batch_reference) == message_batch_post_response_with_origin["data"]["attributes"]["messageBatchReference"]
+        assert message_batch.details == message_batch_post_body_with_origin
+        assert message_batch.response == message_batch_post_response_with_origin
+        assert message_batch.status == models.MessageBatchStatuses.SENT
+
+        assert len(messages) == 1
+        assert messages[0].batch_id == message_batch.id
+        assert messages[0].message_id == message_batch_post_response_with_origin["data"]["attributes"]["messages"][0]["id"]
+        assert str(messages[0].message_reference) == message_batch_post_response_with_origin["data"]["attributes"]["messages"][0]["messageReference"]
+        merged_messages = message_batch_recorder.merged_messages(message_batch_post_body_with_origin, message_batch_post_response_with_origin)
         assert messages[0].details == merged_messages[0]
         assert messages[0].nhs_number == merged_messages[0]["recipient"]["nhsNumber"]
 
